e50578df63e0a2f2c82c47c98e73e1849a51ee60
james austin <j.austin.ford@gmail.com>
2026-09-03 14:47:41 -0600

    add sleep

--- src/__main__.py
+++ src/__main__.py
@@ -5,6 +5,7 @@ backup sources.
 
 import os
 import traceback
+import time
 
 import constants
 import gbackup
@@ -38,6 +39,8 @@ def main():
 
         if os.environ.get("BACKUP_OBJECT").startswith("raid"):
             tpl.smb.unmount(smb_mount)
+
+    time.sleep(50000)
 
 if __name__ == "__main__":
     main()
